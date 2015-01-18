dd8577af8bed49c8817f99303e4f9233a277930e
Sebastian Ross <sebastian.ross@svt.se>
2015-01-18 16:08:31 +0100

    Added gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,24 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    connect = require('gulp-connect');;
 
 gulp.task('lint', function() {
     return gulp.src('polaroidweb.js')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
+
 gulp.task('compress', function() {
     return gulp.src('polaroidweb.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('connect', function() {
+    return connect.server();
 });
 
 gulp.task('default', function() {
